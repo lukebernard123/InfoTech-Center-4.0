2271c39767e397de109a96ffcd5673649e8934a5
Luke Bernard <02luke.bernard@gulllakecs.org>
2023-01-23 14:00:40 -0500

    Update Login Screen

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,13 @@
 # Programmer: Mr.Lange
 # Date: 1.20.2023
 # Program: Infotech Center Upgrades
+
+"""
+Our Welcome Screen will start our program letting
+drivers know that the InfoTechCenter 4.0 is loading
+
+"""
+
+print("\n\n Welcome - InfoTechCenter 4.0")
+
+print("\nInfoTech Center 4.0 is loading")
