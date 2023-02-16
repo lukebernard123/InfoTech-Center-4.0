98706110b0075802aea09eb8203f3a63f679ccb1
Luke Bernard <02luke.bernard@gulllakecs.org>
2023-02-16 13:13:25 -0500

    Made a contact list function.

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,17 @@
-# Programmer: Mr.Lange
-# Date: 1.20.2023
+
+# Programmer: Luke Bernard
+# Date: 2.16.2023
 # Program: Infotech Center Upgrades
+"""
+We will make a function that shows contacts
+- Create a list of contacts
+-Print all contacts
+"""
+# Libraries Here
+import random
+def contacts():
+    contactList = ["Mom","Dad","Brother","Sister","Grandma","Grandpa","Aunt","Uncle",]
+    currentContact = random.choice(contactList)
+    return currentContact
+print("Alert!", contacts(),"is calling.")
+
